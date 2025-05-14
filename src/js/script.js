73288b5908dbd175041ec1746e69d849ce2bf76f
Anton Valkovskyi <u1remited@gmail.com>
2025-05-14 13:14:10 +0300

    header completed (+ img for logo); script for burger menu; created animation.scss;  new mixin for hover (links);

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 import "/src/sass/style.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+	const burger = document.querySelector(".burger"),
+		menu = document.querySelector(".menu"),
+		body = document.body;
+
+	burger.addEventListener("click", () => {
+		const isActive = menu.classList.toggle("menu--active");
+		burger.classList.toggle("burger-close", isActive);
+		body.style.overflow = isActive ? "hidden" : "";
+	});
+});
