9a233baf5cec0aa93b5f98e01d3de930ff134c86
Anton Valkovskyi <u1remited@gmail.com>
2025-05-14 14:11:48 +0300

    fixed bad hover effect for img in logo; added script for correct hover effect for it

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 import "/src/sass/style.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
+    //Burger
 	const burger = document.querySelector(".burger"),
 		menu = document.querySelector(".menu"),
 		body = document.body;
@@ -10,4 +11,16 @@ document.addEventListener("DOMContentLoaded", () => {
 		burger.classList.toggle("burger-close", isActive);
 		body.style.overflow = isActive ? "hidden" : "";
 	});
+    // Logo Img Spinning
+    const logo = document.querySelector(".header__logo"),
+        logoImg = logo.querySelector('img'),
+        [logoAnimation] = logoImg.getAnimations();
+
+    logo.addEventListener("mouseover", () => {
+        logoAnimation.playbackRate = 4;
+    });
+
+    logo.addEventListener("mouseout", () => {
+        logoAnimation.playbackRate = 1;
+    });
 });
