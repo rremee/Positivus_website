305514dafed9bde7c1ce8e8af7d59cb58a2dcc6b
Anton Valkovskyi <u1remited@gmail.com>
2025-05-16 11:38:09 +0300

    hero section; main picture animation (+ 1 rastr img for it); lottie.js added; script for relocate img (media 768)  and initialize lottie player; some changes for button mixin + created universal mixin for text (typography);  changes in text-block component + new common classes for hero heading and text

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 import "/src/sass/style.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
-    //Burger
+	//Burger
 	const burger = document.querySelector(".burger"),
 		menu = document.querySelector(".menu"),
 		body = document.body;
@@ -11,16 +11,49 @@ document.addEventListener("DOMContentLoaded", () => {
 		burger.classList.toggle("burger-close", isActive);
 		body.style.overflow = isActive ? "hidden" : "";
 	});
-    // Logo Img Spinning
-    const logo = document.querySelector(".header__logo"),
-        logoImg = logo.querySelector('img'),
-        [logoAnimation] = logoImg.getAnimations();
 
-    logo.addEventListener("mouseover", () => {
-        logoAnimation.playbackRate = 4;
-    });
+	// Logo Img Spinning
+	const logo = document.querySelector(".header__logo"),
+		logoImg = logo.querySelector("img"),
+		[logoAnimation] = logoImg.getAnimations();
 
-    logo.addEventListener("mouseout", () => {
-        logoAnimation.playbackRate = 1;
-    });
+	logo.addEventListener("mouseover", () => {
+		logoAnimation.playbackRate = 4;
+	});
+
+	logo.addEventListener("mouseout", () => {
+		logoAnimation.playbackRate = 1;
+	});
+
+	// Relocate Hero Picture
+	const heroContainer = document.querySelector(".hero__container"),
+		mediaTabletResponsive = window.matchMedia("(max-width: 767.98px"),
+		heroContent = document.querySelector(".hero__content"),
+		heroPicture = document.querySelector(".hero__picture");
+
+	function pictureRelocate() {
+		if (mediaTabletResponsive.matches) {
+			const heroTitle = heroContent.querySelector(".hero__title");
+			heroContent.insertBefore(heroPicture, heroTitle.nextSibling);
+		} else {
+			heroContainer.appendChild(heroPicture);
+		}
+	}
+
+	mediaTabletResponsive.addEventListener("change", pictureRelocate);
+
+	pictureRelocate();
+
+	// Lottie
+	const lottieAnimation = bodymovin.loadAnimation({
+		container: document.querySelector(".hero__picture"),
+		renderer: "svg",
+		loop: true,
+		autoplay: true,
+		path: "/src/animation/hero/hero-img.json",
+		rendererSettings: {
+			preserveAspectRatio: "xMidYMid meet",
+			progressiveLoad: true
+		},
+	});
 });
