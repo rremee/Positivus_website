23ea256acdfd20344b7100727d4b2640abb3107d
Anton Valkovskyi <u1remited@gmail.com>
2025-06-02 08:43:13 +0300

    +npm plugin-handlebars; changed type to module in package.json + using this plugin in vite config.js; new 'partials' folder for repeating sections (header and footer) + using them in index.html;

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
+import handlebars from 'vite-plugin-handlebars';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+	plugins: [
+		handlebars({
+			partialDirectory: resolve(__dirname, "src/partials")
+		})
+	],
 	build: {
 		rollupOptions: {
 			input: {
