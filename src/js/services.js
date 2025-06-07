ce3d6f3d479cbe66aef2dc288325b8d1a2158874
Anton Valkovskyi <u1remited@gmail.com>
2025-06-07 13:41:37 +0300

    services: hero section + animation for it (+ Lottie)

--- src/js/services.js
+++ src/js/services.js
@@ -1,6 +1,13 @@
 import { initBurger, initLogoSpinning, pictureRelocate, initLottieAnimations } from "./common.js";
+import "/src/sass/blocks/hero-services.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
 	initBurger();
 	initLogoSpinning();
+	initLottieAnimations([{ selector: ".hero__picture", path: "/src/animation/services/services-main.json" }]);
+
+	pictureRelocate("(max-width: 767.98px)", ".hero__wrapper", ".hero__picture", (container, picture) => {
+		const title = container.querySelector(".hero__title");
+		if (title) title.parentNode.insertBefore(picture, title.nextSibling);
+	});
 });
