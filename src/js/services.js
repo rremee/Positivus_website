def2a55a67bc56a222ee8caa16c37c8eb44374da
Anton Valkovskyi <u1remited@gmail.com>
2025-06-08 14:53:39 +0300

    services: ready section + animation for it (+ Lottie + pictureRelocate)

--- src/js/services.js
+++ src/js/services.js
@@ -4,19 +4,27 @@ import "/src/sass/blocks/hero-services.scss";
 document.addEventListener("DOMContentLoaded", () => {
 	initBurger();
 	initLogoSpinning();
-	initLottieAnimations([{ selector: ".hero__picture", path: "/src/animation/services/services-main.json" }]);
+	initLottieAnimations([
+		{ selector: ".hero__picture", path: "/src/animation/services/services-main.json" },
+		{ selector: ".ready__picture", path: "/src/animation/services/services-ready.json" },
+	]);
 
 	pictureRelocate("(max-width: 767.98px)", ".hero__wrapper", ".hero__picture", (container, picture) => {
 		const title = container.querySelector(".hero__title");
 		if (title) title.parentNode.insertBefore(picture, title.nextSibling);
 	});
 
-    pictureRelocate("(max-width: 1199.98px)", ".item-services", ".item-services__picture", (item, picture) => {
+	pictureRelocate("(max-width: 1199.98px)", ".item-services", ".item-services__picture", (item, picture) => {
 		const title = item.querySelector(".item-services__title");
 		if (title && picture) {
 			title.parentNode.insertBefore(picture, title.nextSibling);
 		}
 	});
-    
-    initCaseSliding();
+
+	pictureRelocate("(max-width: 575.98px)", ".ready__wrapper", ".ready__picture", (container, picture) => {
+		const prom = container.querySelector(".ready__prom");
+		if (prom) prom.parentNode.insertBefore(picture, prom.nextSibling);
+	});
+
+	initCaseSliding();
 });
