c6f19a7fe0f558537ebb2840a0d936669edbcf6d
Anton Valkovskyi <u1remited@gmail.com>
2025-06-09 14:11:38 +0300

    cases: added cta section as on the homepage + animation for it (+ Lottie animation + pictureRelocate for this section); some changes in cta.scss (new modifier)

--- src/js/cases.js
+++ src/js/cases.js
@@ -1,16 +1,27 @@
-import { initBurger, initLogoSpinning, pictureRelocate, initLottieAnimations, initTestimonialsSwiper } from "./common.js";
+import {
+	initBurger,
+	initLogoSpinning,
+	pictureRelocate,
+	initLottieAnimations,
+	initTestimonialsSwiper,
+} from "./common.js";
 import "/src/sass/blocks/hero-cases.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
 	initBurger();
 	initLogoSpinning();
-    initLottieAnimations([
-		{ selector: ".hero__picture", path: "/src/animation/cases/cases-main.json" }
+	initLottieAnimations([
+		{ selector: ".hero__picture", path: "/src/animation/cases/cases-main.json" },
+		{ selector: ".cta__picture", path: "/src/animation/cases/cases-cta.json" },
 	]);
 
 	pictureRelocate("(max-width: 767.98px)", ".hero__wrapper", ".hero__picture", (container, picture) => {
 		const title = container.querySelector(".hero__title");
 		if (title) title.parentNode.insertBefore(picture, title.nextSibling);
 	});
-    initTestimonialsSwiper();
+	pictureRelocate("(max-width: 1023.98px)", ".cta__wrapper", ".cta__picture", (container, picture) => {
+		const title = container.querySelector(".cta__title");
+		if (title) title.parentNode.insertBefore(picture, title.nextSibling);
+	});
+	initTestimonialsSwiper();
 });
