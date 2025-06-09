6c4c3fccf0fc62f1c95f0060818f4fd551c4ac76
Anton Valkovskyi <u1remited@gmail.com>
2025-06-09 09:13:36 +0300

    cases: hero section + animation for it (+ Lottie + pictureRelocate)

--- src/js/cases.js
+++ src/js/cases.js
@@ -1,6 +1,15 @@
 import { initBurger, initLogoSpinning, pictureRelocate, initLottieAnimations } from "./common.js";
+import "/src/sass/blocks/hero-cases.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
 	initBurger();
 	initLogoSpinning();
+    initLottieAnimations([
+		{ selector: ".hero__picture", path: "/src/animation/cases/cases-main.json" }
+	]);
+
+	pictureRelocate("(max-width: 767.98px)", ".hero__wrapper", ".hero__picture", (container, picture) => {
+		const title = container.querySelector(".hero__title");
+		if (title) title.parentNode.insertBefore(picture, title.nextSibling);
+	});
 });
