5e0f2b033c02c4c32aa06c3147013ae5966fab12
Anton Valkovskyi <u1remited@gmail.com>
2025-06-10 11:24:26 +0300

    blog: hero section + hero-blog.scss + new var for label; imported some functions for this page (Lottie + pictureRelocate); some changes in plans.scss

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,6 +1,13 @@
-import { initBurger, initLogoSpinning } from "./common.js";
+import { initBurger, initLogoSpinning, initLottieAnimations, pictureRelocate } from "./common.js";
+import "/src/sass/blocks/hero-blog.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
 	initBurger();
 	initLogoSpinning();
+	initLottieAnimations([{ selector: ".hero__picture", path: "/src/animation/hero/hero-img.json" }]);
+
+	pictureRelocate("(max-width: 1023.98px)", ".hero__wrapper", ".hero__picture", (container, picture) => {
+		const title = container.querySelector(".hero__title");
+		if (title) title.parentNode.insertBefore(picture, title.nextSibling);
+	});
 });
